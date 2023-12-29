9d848e94b642540b8a07a1cad448c03ed6888f87
EsharkyTheGreat <eshwars1210@gmail.com>
2023-12-29 19:03:13 +0530

    Fixed Images and Linted Code

--- src/components/Dashboard/DeployedStrategies.jsx
+++ src/components/Dashboard/DeployedStrategies.jsx
@@ -1,14 +1,15 @@
-//import Mascot from "../../public/images/Mascot"
 const DeployedStrategies = () => {
   return (
     <div className="w-[38vw] h-[65vh] flex flex-col py-6 bg-white drop-shadow-md rounded-xl px-8 mt-4 ml-[54px]">
-        <h1 className='text-xl font-bold'>Deployed Strategies</h1>
-        <div className="flex flex-col items-center justify-center mt-36">
-            <img src="" alt="mascot" />
-            <h1 className='text-lg font-regular'>You have no active Deployed Strategies!</h1>
-        </div>
+      <h1 className="text-xl font-bold">Deployed Strategies</h1>
+      <div className="flex flex-col items-center justify-center mt-36">
+        <img src="/mascot.png" alt="mascot" />
+        <h1 className="text-lg font-regular">
+          You have no active Deployed Strategies!
+        </h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DeployedStrategies
+export default DeployedStrategies;
