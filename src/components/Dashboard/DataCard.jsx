eaeba8c7705d74c7d283798cd438ec70d16bc62a
EsharkyTheGreat <eshwars1210@gmail.com>
2023-12-31 15:56:28 +0530

    linting

--- src/components/Dashboard/DataCard.jsx
+++ src/components/Dashboard/DataCard.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import SingleData from "./SingleData"
+import React from "react";
+import SingleData from "./SingleData";
 const DataCard = () => {
   return (
-    <div className='flex flex-col w-[38.5vw] justify-between py-6 bg-white drop-shadow-md rounded-xl px-8 mt-4 ml-[50px]'>
-        <h1 className='text-xl font-bold'>Top Gainer</h1>
-        <SingleData />
-        <SingleData />
-        <SingleData />
+    <div className="flex flex-col w-[38.5vw] justify-between py-6 bg-white drop-shadow-md rounded-xl px-8 mt-4 ml-[50px]">
+      <h1 className="text-xl font-bold">Top Gainer</h1>
+      <SingleData />
+      <SingleData />
+      <SingleData />
     </div>
-  )
-}
+  );
+};
 
-export default DataCard
+export default DataCard;
--- src/components/Dashboard/SecurityCards.jsx
+++ src/components/Dashboard/SecurityCards.jsx
@@ -1,16 +1,16 @@
-import SecurityCard from "./SecurityCard"
+import SecurityCard from "./SecurityCard";
 
 const SecurityCards = () => {
   return (
     <div className="flex flex-row items-center justify-evenly mt-8">
-        <SecurityCard/>
-        <SecurityCard/>
-        <SecurityCard/>
-        <SecurityCard/>
-        <SecurityCard/>
-        <SecurityCard/>
+      <SecurityCard />
+      <SecurityCard />
+      <SecurityCard />
+      <SecurityCard />
+      <SecurityCard />
+      <SecurityCard />
     </div>
-  )
-}
+  );
+};
 
-export default SecurityCards
+export default SecurityCards;
