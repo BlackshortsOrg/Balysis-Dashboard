ee280cf9f3214dce2999e664d7e82a1678cc2d55
Devansh Mishra <79010144+devansh12b2@users.noreply.github.com>
2024-04-07 18:29:42 +0530

    filters fixed

--- src/app/transactionhistory/page.js
+++ src/app/transactionhistory/page.js
@@ -9,206 +9,329 @@ import {
   HoverCardTrigger,
 } from "@/components/ui/hover-card";
 import moment from "moment";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import { Listbox } from "@headlessui/react";
 
-const columns = [
-  // {
-  //   header: "Transaction ID",
-  //   accessorKey: "transaction_id",
-  //   width: "auto",
-  // },
-  {
-    id: "id",
-    accessorKey: "id",
-  },
-  {
-    header: "Time",
-    accessorKey: "created_at",
-    cell: ({ cell }) => {
-      const date = moment(new Date(cell.getValue())).format(
-        "DD/MM/YYYY hh:mm:ss A",
-      );
-      return <p className="">{date}</p>;
-    },
-  },
-  {
-    header: "Symbol",
-    cell: ({ cell, row }) => (
-      <div>
-        {`${row.original.symbol} | ${row.original.segment} | ${row.original.product_type}`}
-      </div>
-    ),
-  },
-  {
-    header: "Order Type",
-    cell: ({ cell, row }) => <div>{row.original.order_type}</div>,
-  },
-  {
-    header: "Limit Price",
-    accessorKey: "limit_price",
-    cell: ({ cell }) => {
-      const val = cell.getValue();
-      return (
-        <div className="text-center">{val == -1 || val == 0 ? "--" : val}</div>
-      );
-    },
-  },
-  {
-    header: "Stop Price",
-    accessorKey: "stop_price",
-    cell: ({ cell }) => {
-      const val = cell.getValue();
-      return (
-        <div className="text-center">{val == -1 || val == 0 ? "--" : val}</div>
-      );
-    },
-  },
-  {
-    header: "Side",
-    accessorKey: "side",
-    cell: ({ cell }) => {
-      return <div>{cell.getValue() == 1 ? "BUY" : "SELL"}</div>;
-    },
-  },
-  // {
-  //   header: "Signal ID",
-  //   accessorKey: "id",
-  //   cell: ({ cell, row }) => {
-  //     console.log(row);
-  //     return (
-  //       <HoverCard>
-  //         <HoverCardTrigger>
-  //           <p className="text-blue-500 cursor-pointer">{cell.getValue()}</p>
-  //         </HoverCardTrigger>
-  //         <HoverCardContent>
-  //           <div className="flex flex-col">
-  //             <p className="text-xs font-semibold">
-  //               Signal Type: {row.original.signal_type}
-  //             </p>
-  //             <p className="text-xs">Order Type: {row.original.order_type}</p>
-  //             <p className="text-xs">Ref ID: {row.original.ref_id}</p>
-  //             <p className="text-xs">
-  //               Ticker:{" "}
-  //               {`${row.original.exchange}:${row.original.symbol}-${row.original.exchange}-${row.original.product_type}`}
-  //             </p>
-  //             <p className="text-xs">Limit Price: {row.original.limit_price}</p>
-  //             <p className="text-xs">Stop Price: {row.original.stop_price}</p>
-  //           </div>
-  //         </HoverCardContent>
-  //       </HoverCard>
-  //     );
-  //   },
-  // },
-  {
-    header: "Name",
-    accessorKey: "name",
-    cell: ({ cell }) => (
-      <div className="flex flex-row items-center mx-1">
-        <Image
-          className="rounded-full"
-          src="/images/dummy.png"
-          width={40}
-          height={40}
-        />
-        <p className="ml-2">{cell.getValue()}</p>
-      </div>
-    ),
-  },
-  {
-    header: "Strategy",
-    accessorKey: "strategy_name",
-  },
-  {
-    header: "Status",
-    accessorKey: "status",
-    cell: ({ cell }) => {
-      switch (cell.getValue()) {
-        case "PENDING":
-          return (
-            <div>
-              <p className="bg-yellow-200 text-yellow-600 font-semibold text-xs border-yellow-600 py-1 border text-center rounded-sm">
-                PENDING
-              </p>
-            </div>
-          );
-        case "TRADED":
-          return (
-            <div>
-              <p className="bg-green-200 text-green-600 font-semibold text-xs border-green-600 py-1 border text-center rounded-sm">
-                TRADED
-              </p>
-            </div>
-          );
-        case "FILLED":
-          return (
-            <div>
-              <p className="bg-green-200 text-green-600 font-semibold text-xs border-green-600 py-1 text-center border rounded-sm">
-                FILLED
-              </p>
-            </div>
-          );
-        case "REJECTED":
-          return (
-            <div>
-              <p className="bg-red-200 text-red-600 font-semibold text-xs border-red-600 py-1 border text-center rounded-sm">
-                REJECTED
-              </p>
-            </div>
-          );
-        case "FAILED":
-          return (
-            <div>
-              <p className="bg-red-200 text-red-600 font-semibold text-xs border-red-600 py-1 border text-center rounded-sm">
-                FAILED
-              </p>
-            </div>
-          );
-        case "CANCELLED":
-          return (
-            <div>
-              <p className="bg-red-200 text-red-600 font-semibold text-xs border-red-600 py-1 text-center border rounded-sm">
-                CANCELLED
-              </p>
-            </div>
-          );
-      }
-    },
-  },
-  {
-    header: "Traded Price",
-    accessorKey: "traded_price",
-    cell: ({ cell }) => {
-      const val = cell.getValue();
-      return (
-        <div className="text-center">{val == -1 || val == 0 ? "--" : val}</div>
-      );
-    },
-  },
-  {
-    header: "Traded Qty",
-    accessorKey: "filledqty",
-    cell: ({ cell }) => {
-      const val = cell.getValue();
-      return (
-        <div className="text-center">{val == -1 || val == 0 ? "--" : val}</div>
-      );
-    },
-  },
-  {
-    header: "Message",
-    accessorKey: "message",
-    maxWidth: "20px",
-    overflow: "auto",
-    cell: ({ cell }) => {
-      return (
-        <div className="max-w-[100px] max-h-[40px] overflow-auto">
-          {cell.getValue()}
-        </div>
-      );
-    },
-  },
-];
+
 
 const transactionhistory = () => {
+  const [daily, setDaily] = useState(true);
+  const [data, setData] = useState([]);
+
+  const all_strategies = [
+    "",
+    ...new Set(data.map((item) => item.strategy_name)),
+  ];
+  const status_options = ["PENDING", "TRADED", "REJECTED", "FAILED", ""];
+  const all_users = ["", ...new Set(data.map((item) => item.name))];
+
+  const columns = [
+    // {
+    //   header: "Transaction ID",
+    //   accessorKey: "transaction_id",
+    //   width: "auto",
+    // },
+    {
+      id: "id",
+      accessorKey: "id",
+    },
+    {
+      header: "Time",
+      accessorKey: "created_at",
+      cell: ({ cell }) => {
+        const date = moment(new Date(cell.getValue())).format(
+          "DD/MM/YYYY hh:mm:ss A"
+        );
+        return <p className="">{date}</p>;
+      },
+    },
+    {
+      header: "Symbol",
+      cell: ({ cell, row }) => (
+        <div>
+          {`${row.original.symbol} | ${row.original.segment} | ${row.original.product_type}`}
+        </div>
+      ),
+    },
+    {
+      header: "Order Type",
+      cell: ({ cell, row }) => <div>{row.original.order_type}</div>,
+    },
+    {
+      header: "Limit Price",
+      accessorKey: "limit_price",
+      cell: ({ cell }) => {
+        const val = cell.getValue();
+        return (
+          <div className="text-center">
+            {val == -1 || val == 0 ? "--" : val}
+          </div>
+        );
+      },
+    },
+    {
+      header: "Stop Price",
+      accessorKey: "stop_price",
+      cell: ({ cell }) => {
+        const val = cell.getValue();
+        return (
+          <div className="text-center">
+            {val == -1 || val == 0 ? "--" : val}
+          </div>
+        );
+      },
+    },
+    {
+      header: "Side",
+      accessorKey: "side",
+      cell: ({ cell }) => {
+        return <div>{cell.getValue() == 1 ? "BUY" : "SELL"}</div>;
+      },
+    },
+    // {
+    //   header: "Signal ID",
+    //   accessorKey: "id",
+    //   cell: ({ cell, row }) => {
+    //     console.log(row);
+    //     return (
+    //       <HoverCard>
+    //         <HoverCardTrigger>
+    //           <p className="text-blue-500 cursor-pointer">{cell.getValue()}</p>
+    //         </HoverCardTrigger>
+    //         <HoverCardContent>
+    //           <div className="flex flex-col">
+    //             <p className="text-xs font-semibold">
+    //               Signal Type: {row.original.signal_type}
+    //             </p>
+    //             <p className="text-xs">Order Type: {row.original.order_type}</p>
+    //             <p className="text-xs">Ref ID: {row.original.ref_id}</p>
+    //             <p className="text-xs">
+    //               Ticker:{" "}
+    //               {`${row.original.exchange}:${row.original.symbol}-${row.original.exchange}-${row.original.product_type}`}
+    //             </p>
+    //             <p className="text-xs">Limit Price: {row.original.limit_price}</p>
+    //             <p className="text-xs">Stop Price: {row.original.stop_price}</p>
+    //           </div>
+    //         </HoverCardContent>
+    //       </HoverCard>
+    //     );
+    //   },
+    // },
+    {
+      header: ({ table }) => (
+        <div className="flex flex-row">
+          <div>Name</div>
+          <div className="ml-2">
+            <div className="relative">
+              <Listbox
+                value={table.getColumn("name")?.getFilterValue() ?? ""}
+                onChange={(value) => {
+                  table.getColumn("name")?.setFilterValue(value);
+                }}
+              >
+                <Listbox.Button className="border shadow-sm rounded-md px-2 py-1 flex flex-col">
+                  <FontAwesomeIcon icon={faFilter} />
+                </Listbox.Button>
+                <Listbox.Options className="absolute border-2 border-blue-400 z-10 mt-1 bg-white rounded-md shadow-lg text-center">
+                  {all_users.map((option) => (
+                    <Listbox.Option
+                      className={`p-1 border-1 uppercase rounded-md cursor-pointer ${
+                        option ===
+                        table.getColumn("strategy_name")?.getFilterValue()
+                          ? "bg-blue-400 text-white"
+                          : "hover:bg-blue-400 hover:text-white"
+                      }`}
+                      key={option}
+                      value={option}
+                    >
+                      {option ? option : "None"}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              </Listbox>
+            </div>
+          </div>
+        </div>
+      ),
+      accessorKey: "name",
+      cell: ({ cell }) => (
+        <div className="flex flex-row items-center mx-1">
+          <Image
+            className="rounded-full"
+            src="/images/dummy.png"
+            width={40}
+            height={40}
+          />
+          <p className="ml-2">{cell.getValue()}</p>
+        </div>
+      ),
+    },
+    {
+      header: ({ table }) => (
+        <div className="flex flex-row">
+          <div>Strategy</div>
+          <div className="ml-2">
+            <div className="relative">
+              <Listbox
+                value={table.getColumn("strategy_name")?.getFilterValue() ?? ""}
+                onChange={(value) => {
+                  table.getColumn("strategy_name")?.setFilterValue(value);
+                }}
+              >
+                <Listbox.Button className="border shadow-sm rounded-md px-2 py-1 flex flex-col">
+                  <FontAwesomeIcon icon={faFilter} />
+                </Listbox.Button>
+                <Listbox.Options className="absolute border-2 border-blue-400 z-10 mt-1 bg-white rounded-md shadow-lg text-center">
+                  {all_strategies.map((option) => (
+                    <Listbox.Option
+                      className={`p-1 border-1 uppercase rounded-md cursor-pointer ${
+                        option ===
+                        table.getColumn("strategy_name")?.getFilterValue()
+                          ? "bg-blue-400 text-white"
+                          : "hover:bg-blue-400 hover:text-white"
+                      }`}
+                      key={option}
+                      value={option}
+                    >
+                      {option ? option : "None"}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              </Listbox>
+            </div>
+          </div>
+        </div>
+      ),
+      accessorKey: "strategy_name",
+    },
+    {
+      header: ({ table }) => (
+        <div className="flex flex-row">
+          <div>Status</div>
+          <div className="ml-2">
+            <div className="relative">
+              <Listbox
+                value={table.getColumn("status")?.getFilterValue() ?? ""}
+                onChange={(value) => {
+                  table.getColumn("status")?.setFilterValue(value);
+                }}
+              >
+                <Listbox.Button className="border shadow-sm rounded-md px-2 py-1 flex flex-col">
+                  <FontAwesomeIcon icon={faFilter} />
+                </Listbox.Button>
+                <Listbox.Options className="absolute border-2 border-blue-400 z-10 mt-1 bg-white rounded-md shadow-lg text-center">
+                  {status_options.map((option) => (
+                    <Listbox.Option
+                      className={`p-1 border-1 uppercase rounded-md cursor-pointer ${
+                        option === table.getColumn("status")?.getFilterValue()
+                          ? "bg-blue-400 text-white"
+                          : "hover:bg-blue-400 hover:text-white"
+                      }`}
+                      key={option}
+                      value={option}
+                    >
+                      {option ? option : "None"}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              </Listbox>
+            </div>
+          </div>
+        </div>
+      ),
+      accessorKey: "status",
+      cell: ({ cell }) => {
+        switch (cell.getValue()) {
+          case "PENDING":
+            return (
+              <div>
+                <p className="bg-yellow-200 text-yellow-600 font-semibold text-xs border-yellow-600 py-1 border text-center rounded-sm">
+                  PENDING
+                </p>
+              </div>
+            );
+          case "TRADED":
+            return (
+              <div>
+                <p className="bg-green-200 text-green-600 font-semibold text-xs border-green-600 py-1 border text-center rounded-sm">
+                  TRADED
+                </p>
+              </div>
+            );
+          case "FILLED":
+            return (
+              <div>
+                <p className="bg-green-200 text-green-600 font-semibold text-xs border-green-600 py-1 text-center border rounded-sm">
+                  FILLED
+                </p>
+              </div>
+            );
+          case "REJECTED":
+            return (
+              <div>
+                <p className="bg-red-200 text-red-600 font-semibold text-xs border-red-600 py-1 border text-center rounded-sm">
+                  REJECTED
+                </p>
+              </div>
+            );
+          case "FAILED":
+            return (
+              <div>
+                <p className="bg-red-200 text-red-600 font-semibold text-xs border-red-600 py-1 border text-center rounded-sm">
+                  FAILED
+                </p>
+              </div>
+            );
+          case "CANCELLED":
+            return (
+              <div>
+                <p className="bg-red-200 text-red-600 font-semibold text-xs border-red-600 py-1 text-center border rounded-sm">
+                  CANCELLED
+                </p>
+              </div>
+            );
+        }
+      },
+    },
+    {
+      header: "Traded Price",
+      accessorKey: "traded_price",
+      cell: ({ cell }) => {
+        const val = cell.getValue();
+        return (
+          <div className="text-center">
+            {val == -1 || val == 0 ? "--" : val}
+          </div>
+        );
+      },
+    },
+    {
+      header: "Traded Qty",
+      accessorKey: "filledqty",
+      cell: ({ cell }) => {
+        const val = cell.getValue();
+        return (
+          <div className="text-center">
+            {val == -1 || val == 0 ? "--" : val}
+          </div>
+        );
+      },
+    },
+    {
+      header: "Message",
+      accessorKey: "message",
+      maxWidth: "20px",
+      overflow: "auto",
+      cell: ({ cell }) => {
+        return (
+          <div className="max-w-[100px] max-h-[40px] overflow-auto">
+            {cell.getValue()}
+          </div>
+        );
+      },
+    },
+  ];
   async function checkLogin() {
     if (localStorage.getItem("token") === null) {
       window.location.href = "/login";
@@ -217,8 +340,7 @@ const transactionhistory = () => {
     }
   }
 
-  const [daily, setDaily] = useState(true);
-  const [data, setData] = useState([]);
+  
   async function callAPI(token) {
     const data = await getTransactionHistoryAPI(token, daily);
     setData(data);
--- src/components/TransactionHistory/table.jsx
+++ src/components/TransactionHistory/table.jsx
@@ -22,8 +22,6 @@ import {
 import { Listbox } from "@headlessui/react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-const status_options = ["PENDING", "TRADED", "REJECTED", "FAILED", ""];
-
 export function TxTable({ columns, data, daily, setDaily }) {
   const [rowSelection, setRowSelection] = React.useState({});
   const [selectedFilterStatus, setSelectedFilterStatus] = React.useState();
@@ -53,7 +51,6 @@ export function TxTable({ columns, data, daily, setDaily }) {
     enableRowSelection: true,
   });
 
-  const all_users = ["", ...new Set(data.map((item) => item.name))];
   const all_strategies = [
     "",
     ...new Set(data.map((item) => item.strategy_name)),
@@ -61,96 +58,28 @@ export function TxTable({ columns, data, daily, setDaily }) {
 
   return (
     <div>
-      <div className="flex flex-row">
-        <div className="relative">
-          <Listbox
-            value={table.getColumn("strategy_name")?.getFilterValue() ?? ""}
-            onChange={(value) => {
-              table.getColumn("strategy_name")?.setFilterValue(value);
-            }}
-          >
-            <Listbox.Button className="border shadow-sm rounded-md px-2 py-1 flex flex-col">
-              Filter Strategies{" "}
-              {table.getColumn("strategy_name")?.getFilterValue() ?? " "}
-            </Listbox.Button>
-            <Listbox.Options className="absolute border-2 border-blue-400 z-10 mt-1 bg-white rounded-md shadow-lg text-center">
-              {all_strategies.map((option) => (
-                <Listbox.Option
-                  className="p-1 border-1 uppercase rounded-md cursor-pointer on hover:bg-blue-400 hover:text-white"
-                  key={option}
-                  value={option}
-                >
-                  {option ? option : "None"}
-                </Listbox.Option>
-              ))}
-            </Listbox.Options>
-          </Listbox>
-        </div>
-        <div className="relative">
-          <Listbox
-            value={table.getColumn("status")?.getFilterValue() ?? ""}
-            onChange={(value) => {
-              table.getColumn("status")?.setFilterValue(value);
-            }}
-          >
-            <Listbox.Button className="border shadow-sm rounded-md px-2 py-1 flex flex-col">
-              Filter Status {table.getColumn("status")?.getFilterValue() ?? " "}
-            </Listbox.Button>
-            <Listbox.Options className="absolute border-2 border-blue-400 z-10 mt-1 bg-white rounded-md shadow-lg text-center">
-              {status_options.map((option) => (
-                <Listbox.Option
-                  className="p-1 border-1 rounded-md cursor-pointer on hover:bg-blue-400 hover:text-white"
-                  key={option}
-                  value={option}
-                >
-                  {option ? option : "None"}
-                </Listbox.Option>
-              ))}
-            </Listbox.Options>
-          </Listbox>
-        </div>
-        <div className="">
-          <Listbox
-            value={table.getColumn("name")?.getFilterValue() ?? ""}
-            onChange={(value) => {
-              table.getColumn("name")?.setFilterValue(value);
-            }}
-          >
-            <Listbox.Button className="border shadow-sm rounded-md px-2 py-1 flex flex-col">
-              Filter Name {table.getColumn("name")?.getFilterValue() ?? " "}
-            </Listbox.Button>
-            <Listbox.Options className="absolute border-2 border-blue-400 z-10 mt-1 bg-white rounded-md shadow-lg text-center">
-              {all_users.map((option) => (
-                <Listbox.Option
-                  className="p-1 border-1 rounded-md cursor-pointer on hover:bg-blue-400 hover:text-white"
-                  key={option}
-                  value={option}
-                >
-                  {option ? option : "None"}
-                </Listbox.Option>
-              ))}
-            </Listbox.Options>
-          </Listbox>
-        </div>
-        <div className="">
-          <Input
-            placeholder="Filter messages..."
-            value={table.getColumn("message")?.getFilterValue() ?? ""}
-            onChange={(event) =>
-              table.getColumn("message")?.setFilterValue(event.target.value)
-            }
-            className="max-w-sm"
-          />
-        </div>
-        <div className="">
-          <Input
-            placeholder="Filter signals..."
-            value={table.getColumn("id")?.getFilterValue() ?? ""}
-            onChange={(event) =>
-              table.getColumn("id")?.setFilterValue(event.target.value)
-            }
-            className="max-w-sm"
-          />
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-row">
+          <div className="">
+            <Input
+              placeholder="Filter messages..."
+              value={table.getColumn("message")?.getFilterValue() ?? ""}
+              onChange={(event) =>
+                table.getColumn("message")?.setFilterValue(event.target.value)
+              }
+              className="max-w-sm"
+            />
+          </div>
+          <div className="">
+            <Input
+              placeholder="Filter signals..."
+              value={table.getColumn("id")?.getFilterValue() ?? ""}
+              onChange={(event) =>
+                table.getColumn("id")?.setFilterValue(event.target.value)
+              }
+              className="max-w-sm"
+            />
+          </div>
         </div>
         <div className="text-right">
           <Tabs
@@ -169,7 +98,7 @@ export function TxTable({ columns, data, daily, setDaily }) {
         </div>
       </div>
       <div className="rounded-md border">
-        <Table>
+        <Table className="h-full">
           <TableHeader>
             {table.getHeaderGroups().map((headerGroup) => (
               <TableRow key={headerGroup.id}>
@@ -180,7 +109,7 @@ export function TxTable({ columns, data, daily, setDaily }) {
                         ? null
                         : flexRender(
                             header.column.columnDef.header,
-                            header.getContext(),
+                            header.getContext()
                           )}
                     </TableHead>
                   );
@@ -188,7 +117,7 @@ export function TxTable({ columns, data, daily, setDaily }) {
               </TableRow>
             ))}
           </TableHeader>
-          <TableBody>
+          <TableBody className="h-[69vh]">
             {table.getRowModel().rows?.length ? (
               table.getRowModel().rows.map((row) => (
                 <TableRow
@@ -200,7 +129,7 @@ export function TxTable({ columns, data, daily, setDaily }) {
                     <TableCell key={cell.id}>
                       {flexRender(
                         cell.column.columnDef.cell,
-                        cell.getContext(),
+                        cell.getContext()
                       )}
                     </TableCell>
                   ))}
